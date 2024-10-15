d4b43d77dddfe0119b3dac63359015ca8db7be50
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-10-15 13:15:45 +0545

    fixes

--- server.js
+++ server.js
@@ -22,9 +22,17 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Bhansha Express');
+});
+
+
+
 // Error handling middleware
 app.use(errorHandler);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
