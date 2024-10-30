a08975a3247332b8901a36490f911d350ed3a972
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-10-30 16:18:27 +0545

    file checking

---
+++ models/Order.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  orderId: { type: String, unique: true, required: true }, //order id 
+  items: [
+    {
+      title: { type: String, required: true },
+      price: { type: Number, required: true },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  totalPrice: { type: Number, required: true },
+  user: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    location: { type: String, required: true },
+    sentDate: { type: Date, default: Date.now },
+  },
+  status: {
+    type: String,
+    enum: [
+      'Not Set',
+      'Cooking',
+      'On Delivery',
+      'Delivery Complete',
+      'Order Cancel'
+    ],
+    default: 'Not Set'
+  },
+  paymentMethod: {
+    type: String,
+    enum: [
+      'Cash on Delivery',
+      'Takeout'
+    ],
+    required: true
+
+  },
+  takeoutLocation: {
+    type: String,
+    default: null
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Order', OrderSchema);
