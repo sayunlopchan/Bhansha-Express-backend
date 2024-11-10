b23dff9173a61a0e4aae5501285ac4f3b2479d4d
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-11-10 16:51:37 +0545

    path

--- constant/path.js
+++ constant/path.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 const BASEURL = process.env.BASEURL || 'http://localhost:5000';
 
 // Common Paths
-const IMAGE_PATH = `${BASEURL}/api/image`;
+const IMAGE_PATH = `${BASEURL}/assets/email`;
 
 // Helper function for image URLs
-const getImageUrl = (id) => `${IMAGE_PATH}/${id}`;
+const getImageUrl = (name) => `${IMAGE_PATH}/${name}`;
 
 // Assets 
-const LOGO = getImageUrl("67210b13cf6136f012411478");
-const BANNER = getImageUrl("671f7c6b1139c9d43848a9d8");
+const LOGO = getImageUrl("logo.png");
+const BANNER = getImageUrl("banner.png");
 
 // Social Media Links
 const INSTAGRAM = "https://www.instagram.com/bhanshaexpress";
@@ -23,21 +23,21 @@ const GOOGLEMAP = "https://maps.app.goo.gl/13bcbjHbvk8z7TeC7";
 const LOCATION = "Butwal";
 
 // Social Media Icons
-const INSTAGRAM_ICON = getImageUrl("67211686cf6136f01241147c");
-const FACEBOOK_ICON = getImageUrl("672115e4cf6136f01241147a");
-const WHATSAPP_ICON = getImageUrl("672116a5cf6136f01241147e");
+const INSTAGRAM_ICON = getImageUrl("instagram.png");
+const FACEBOOK_ICON = getImageUrl("facebook.png");
+const WHATSAPP_ICON = getImageUrl("whatsapp.png");
 
 // Status Images
 const STATUS_IMAGES = {
-  ON_DELIVERY: getImageUrl("6721d2697170de730f4be6df"),
-  DELIVERED: getImageUrl("6721d3cc71209ca14a920d5d"),
-  CANCELED: getImageUrl("6721d2237170de730f4be6c8"),
+  ON_DELIVERY: getImageUrl("shipping.png"),
+  DELIVERED: getImageUrl("complete.png"),
+  CANCELED: getImageUrl("cancelled.png"),
 };
 
 // Progress Icon Images
 const PROGRESS_ICONS = {
-  READY_TO_COOK: getImageUrl("6720af474d23b0f2c87b7e60"),
-  ORDER_CONFIRM: getImageUrl("6720af404d23b0f2c87b7e5e"),
+  READY_TO_COOK: getImageUrl("cooking.png"),
+  ORDER_CONFIRM: getImageUrl("confirmation.png"),
 };
 
 // Export the configuration
